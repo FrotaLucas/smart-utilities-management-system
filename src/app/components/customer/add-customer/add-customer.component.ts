4375a283886dcc3d0ff5347e3842d548fe686c7a
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-09 20:04:45 +0200

    new comments to review

--- src/app/components/customer/add-customer/add-customer.component.ts
+++ src/app/components/customer/add-customer/add-customer.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-customer',
-  standalone: true,
-  imports: [RouterModule],
+  //standalone: true,
+  imports: [],  //it works without CommomMOdule and RouterModule
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css'
 })
