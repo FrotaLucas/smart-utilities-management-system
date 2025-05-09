58d74429a8826b89d08d66aa464c79863ed64fe7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-09 17:03:19 +0200

    fix routes to standalone app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appRouterProviders } from './app/app.routes';
+
 
 //standalone application
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+//standalone application
+//nao precisa usar o appConfig ??????????
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appRouterProviders
+  ]
+}).catch( err => console.log(err));
