12c9993b76c58a0357054770f5cb514aed4ff79c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-09 14:51:04 +0200

    comments to standalone app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
+
+//I am using standalone arquitecture
+//benefits:
+//Suitable for small, modular, or isolated components.
+//obs: Angule Module is a different approach can be better than standalone in case 
+//large enterprise applications like E-commmerce Platform.
+
 export const routes: Routes = [];
