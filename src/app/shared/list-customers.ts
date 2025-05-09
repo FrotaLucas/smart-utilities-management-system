9bc4507bd3368a1d7c01adfad8ac00d017894d74
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-09 20:27:04 +0200

    create list of Customer

---
+++ src/app/shared/list-customers.ts
@@ -0,0 +1,34 @@
+import { Customer } from "../interfaces/customer";
+
+
+const listOfCustomers: Customer[] = [
+    {
+        name: 'Lucas',
+        familyname: 'Dias',
+        age: 32
+    },
+     {
+        name: 'Lucas',
+        familyname: 'Dias',
+        age: 32
+    },
+     {
+        name: 'Lucas',
+        familyname: 'Dias',
+        age: 32
+    },
+     {
+        name: 'Lucas',
+        familyname: 'Dias',
+        age: 32
+    },
+     {
+        name: 'Lucas',
+        familyname: 'Dias',
+        age: 32
+    }
+
+];
+
+
+export {listOfCustomers};
