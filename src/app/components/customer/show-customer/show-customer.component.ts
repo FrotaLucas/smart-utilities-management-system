95414cb66abe52b8c592bc8106f16b6f2c6b52a0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-11 14:32:42 +0200

    logic for button

--- src/app/components/customer/show-customer/show-customer.component.ts
+++ src/app/components/customer/show-customer/show-customer.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { Customer } from '../../../interfaces/customer';
 import { listOfCustomers } from '../../../shared/list-customers';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-customer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './show-customer.component.html',
   styleUrl: './show-customer.component.css'
 })
 export class ShowCustomerComponent {
 
   myList: Customer[] = listOfCustomers;
-
+  nameTest: string = "mensage";
 
   ngOnit() : void{
     this.refreshPage();
@@ -19,5 +20,10 @@ export class ShowCustomerComponent {
 
   refreshPage() {
     this.myList = listOfCustomers;
+  }
+
+  public eventClick() {
+    this.nameTest = "New Name";
+    console.log(this.nameTest);
   }
 }
