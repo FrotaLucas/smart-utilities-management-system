6ddbc148f729daea0132a01fdc19252302f6a47c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-11 11:04:17 +0200

    add correct Component

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { ShowCustomerComponent } from './show-customer/show-customer.component';
 
         // ADCIONAR COMMOM TBM NOS IMPORTS
 @Component({
   selector: 'app-customer',
   //standalone: true,
-  imports: [AddCustomerComponent],  //it works without CommonModule and RouterModule
+  imports: [AddCustomerComponent, ShowCustomerComponent],  //it works without CommonModule and RouterModule
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
