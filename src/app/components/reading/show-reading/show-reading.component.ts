380026209a99a3f74cf9489b328fc671fe7ccbbc
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-12 15:40:58 +0200

    fix string

--- src/app/components/reading/show-reading/show-reading.component.ts
+++ src/app/components/reading/show-reading/show-reading.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Reading } from '../../../interfaces/reading';
 import { listOfreading } from '../../../shared/list-readings';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-show-reading',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './show-reading.component.html',
   styleUrl: './show-reading.component.css'
 })
@@ -13,12 +14,14 @@ export class ShowReadingComponent {
 
   myReading: Reading[] = listOfreading;
 
+  constructor(private router: Router){}
+
   ngOnInit(): void {
     this.myReading = listOfreading;
   }
 
   navigateTo(): void {
-
+    this.router.navigate(['/reading/add']);
   }
 
 }
