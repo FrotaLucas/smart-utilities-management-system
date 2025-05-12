c179039a431fa459f6070cc958e50edb7bf4a32a
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-12 13:58:45 +0200

    import ShowReadingComponent

--- src/app/components/reading/reading.component.ts
+++ src/app/components/reading/reading.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ShowReadingComponent } from './show-reading/show-reading.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-reading',
   //standalone: true,
-  imports: [], //it works without CommomMOdule and RouterModule
+  imports: [ShowReadingComponent], //it works without CommomMOdule and RouterModule
   templateUrl: './reading.component.html',
   styleUrl: './reading.component.css'
 })
