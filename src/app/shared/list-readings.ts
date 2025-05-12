22ff1a22256ce5c7be8edefa794005a8c27f63d1
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-12 14:28:26 +0200

    create list of Readings

---
+++ src/app/shared/list-readings.ts
@@ -0,0 +1,42 @@
+import { Reading } from "../interfaces/reading"
+
+const listOfreading: Reading[] = [
+  {
+    kindOfMeter: "Energy",
+    comment: "Routine monthly reading",
+    meterId: 101,
+    meterCount: 15230,
+    dateOfReading: "2025-05-01"
+  },
+  {
+    kindOfMeter: "Water",
+    comment: "Slightly higher than usual",
+    meterId: 202,
+    meterCount: 847,
+    dateOfReading: "2025-05-01"
+  },
+  {
+    kindOfMeter: "Energy",
+    comment: "After maintenance",
+    meterId: 101,
+    meterCount: 15400,
+    dateOfReading: "2025-06-01"
+  },
+  {
+    kindOfMeter: "Water",
+    comment: "Normal usage",
+    meterId: 202,
+    meterCount: 900,
+    dateOfReading: "2025-06-01"
+  },
+  {
+    kindOfMeter: "Energy",
+    comment: "New tenant started",
+    meterId: 103,
+    meterCount: 500,
+    dateOfReading: "2025-05-10"
+  }
+];
+
+export {listOfreading};
+
