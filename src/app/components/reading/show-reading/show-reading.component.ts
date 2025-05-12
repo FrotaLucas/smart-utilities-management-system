0d1a010a47204d58f70040f6443e0c08848054a6
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-12 15:26:53 +0200

    import packages to showreading component

--- src/app/components/reading/show-reading/show-reading.component.ts
+++ src/app/components/reading/show-reading/show-reading.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reading } from '../../../interfaces/reading';
+import { listOfreading } from '../../../shared/list-readings';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-reading',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './show-reading.component.html',
   styleUrl: './show-reading.component.css'
 })
 export class ShowReadingComponent {
+
+  myReading: Reading[] = listOfreading;
+
+  ngOnInit(): void {
+    this.myReading = listOfreading;
+  }
+
+  navigateTo(): void {
+
+  }
 
 }
