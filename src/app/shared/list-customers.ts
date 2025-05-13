e95f3fd0dc8a711b438f065df6cf31bbe125f062
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-13 20:39:45 +0200

    fix field id

--- src/app/shared/list-customers.ts
+++ src/app/shared/list-customers.ts
@@ -3,36 +3,40 @@ import { Customer } from "../interfaces/customer";
 
 const listOfCustomers: Customer[] = [
     {
+        id: 1,
         name: 'Name',
         familyName: 'Surname',
         gender: "male",
         birthDate: "01-02-1995"
     },
-     {
+    {
+        id: 2,
         name: 'Name',
         familyName: 'Surname',
         gender: "male",
         birthDate: "01-02-1995"
     },
-     {
+    {
+        id: 3,
         name: 'Name',
         familyName: 'Surname',
         gender: "male",
         birthDate: "01-02-1995"
     },
-     {
+    {
+        id: 4,
         name: 'Name',
         familyName: 'Surname',
         gender: "male",
         birthDate: "01-02-1995"
     },
-     {
+    {
+        id: 5,
         name: 'Name',
         familyName: 'Surname',
         gender: "male",
         birthDate: "01-02-1995"
     }
-
 ];
 
 
