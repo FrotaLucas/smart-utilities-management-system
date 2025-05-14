74e45683599f259da9e932f024f2d0a796546e6b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-14 19:39:27 +0200

    add HttpCLient

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { appRouterProviders } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 
 //standalone application
@@ -12,6 +13,6 @@ import { appRouterProviders } from './app/app.routes';
 //nao precisa usar o appConfig ??????????
 bootstrapApplication(AppComponent, {
   providers: [
-    ...appRouterProviders
+    ...appRouterProviders, provideHttpClient()
   ]
 }).catch( err => console.log(err));
