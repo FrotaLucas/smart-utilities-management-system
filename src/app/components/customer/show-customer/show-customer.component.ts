88021a9189ff56ffbb5652e40dee5451e3cfa91c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-14 21:48:25 +0200

    refactoring delete method

--- src/app/components/customer/show-customer/show-customer.component.ts
+++ src/app/components/customer/show-customer/show-customer.component.ts
@@ -18,7 +18,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-show-customer',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule], 
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './show-customer.component.html',
   styleUrl: './show-customer.component.css'
 })
@@ -29,50 +29,59 @@ export class ShowCustomerComponent {
   myList: Customer[] = list;
   nameTest: string = "mensage";
 
-  constructor (private router: Router, private dialog: MatDialog, private _customerService: CustomerService){}
+  constructor(private router: Router, private dialog: MatDialog, private _customerService: CustomerService) { }
 
-  ngOnInit() : void{  //deveria importar o metodo ngOnit !!
+  ngOnInit(): void {  //deveria importar o metodo ngOnit !!
     this.refreshPage();
   }
 
   refreshPage() {
-    this.myList = list;
-    this._customerService.getCustomers().subscribe( data =>{
+    this._customerService.getCustomers().subscribe(data => {
       this.listOfCustomers = data;
       console.log('list', this.listOfCustomers)
     }
     );
-    
+
   }
 
   public navigateTo() {
     this.router.navigate(['/customer/add']);
   }
 
-  deleteCustomer(id?: number): void {
-    this.myList = this.myList.filter(item => item.id !== id);
+  // deleteCustomer(id?: number): void {
+  //   this.myList = this.myList.filter(item => item.id !== id);
+  // }
+
+  deleteCustomer(uuid: string): void {
+
+    this._customerService.deleteCustomer(uuid).subscribe(customer =>
+      console.log('deleted customer', customer)
+    );
+
+    this.refreshPage();
   }
 
+
   editCustomer(id?: number): void {
-      const customer = this.myList.find( item => item.id === id);
+    const customer = this.myList.find(item => item.id === id);
 
-      //dialog passa o customer dentro do campo data
-      const dialogRef = this.dialog.open(EditCustomerComponent, {
-        width: '600px',
-        data: customer
-      });
+    //dialog passa o customer dentro do campo data
+    const dialogRef = this.dialog.open(EditCustomerComponent, {
+      width: '600px',
+      data: customer
+    });
 
-      //dialogRef recebe de volta o customer editado
-     dialogRef.afterClosed().subscribe(result => {
-        if(result){
+    //dialogRef recebe de volta o customer editado
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
 
-          //encontar o indice na lista Mylist
-          const index = this.myList.findIndex(item => item.id === id);
-          //se encontrar algo
-          if(index !== -1){
-            this.myList[index] = result;
-          }
+        //encontar o indice na lista Mylist
+        const index = this.myList.findIndex(item => item.id === id);
+        //se encontrar algo
+        if (index !== -1) {
+          this.myList[index] = result;
         }
-     })
+      }
+    })
   }
 }
