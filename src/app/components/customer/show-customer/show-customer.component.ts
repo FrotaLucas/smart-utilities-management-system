e69a4c507feae1eea8d02f3a91d21701af6ce8ef
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-14 12:06:26 +0200

    add deleteCustomer and editCustomer methods

--- src/app/components/customer/show-customer/show-customer.component.ts
+++ src/app/components/customer/show-customer/show-customer.component.ts
@@ -4,15 +4,19 @@ import { listOfCustomers } from '../../../shared/list-customers';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 
+import { EditCustomerComponent } from '../edit-customer/edit-customer.component';
+
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-show-customer',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './show-customer.component.html',
   styleUrl: './show-customer.component.css'
 })
@@ -21,7 +25,7 @@ export class ShowCustomerComponent {
   myList: Customer[] = listOfCustomers;
   nameTest: string = "mensage";
 
-  constructor (private router: Router){}
+  constructor (private router: Router, private dialog: MatDialog){}
 
   ngOnInit() : void{  //deveria importar o metodo ngOnit !!
     this.refreshPage();
@@ -35,5 +39,32 @@ export class ShowCustomerComponent {
 
   public navigateTo() {
     this.router.navigate(['/customer/add']);
+  }
+
+  deleteCustomer(id?: number): void {
+    this.myList = this.myList.filter(item => item.id !== id);
+  }
+
+  editCustomer(id?: number): void {
+      const customer = this.myList.find( item => item.id === id);
+
+      //dialog passa o customer dentro do campo data
+      const dialogRef = this.dialog.open(EditCustomerComponent, {
+        width: '600px',
+        data: customer
+      });
+
+      //dialogRef recebe de volta o customer editado
+     dialogRef.afterClosed().subscribe(result => {
+        if(result){
+
+          //encontar o indice na lista Mylist
+          const index = this.myList.findIndex(item => item.id === id);
+          //se encontrar algo
+          if(index !== -1){
+            this.myList[index] = result;
+          }
+        }
+     })
   }
 }
