0a1d09b1d3d023d01698fe85b290f7891937e662
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-15 17:12:46 +0200

    implement dateFormat

--- src/app/components/reading/edit-reading/edit-reading.component.ts
+++ src/app/components/reading/edit-reading/edit-reading.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Reading } from '../../../interfaces/reading'
+import { ReadingService } from '../../../services/reading.service';
 
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -29,17 +30,18 @@ import { MatRadioModule } from '@angular/material/radio';
 })
 
 
-export class EditReadingComponent implements OnInit{
+export class EditReadingComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) private reading: Reading, 
-  private dialogRef: MatDialogRef<EditReadingComponent>) {
+  constructor(private fb: FormBuilder, private _readingService: ReadingService,
+    @Inject(MAT_DIALOG_DATA) private reading: Reading,
+    private dialogRef: MatDialogRef<EditReadingComponent>) {
 
   }
 
   ngOnInit(): void {
-       this.form = this.fb
+    this.form = this.fb
       .group({
         meterId: [this.reading.meterId, Validators.required],
         kindOfMeter: [this.reading.kindOfMeter, Validators.required],
@@ -50,6 +52,13 @@ export class EditReadingComponent implements OnInit{
       })
   }
 
+  formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // mês começa em 0
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+  }
+
   saveChanges(): void {
     if (this.form.valid) {
       this.reading.meterId = this.form.value.meterId;
@@ -57,11 +66,11 @@ export class EditReadingComponent implements OnInit{
       this.reading.meterCount = this.form.value.meterCount;
       this.reading.comment = this.form.value.comment;
       this.reading.substitute = this.form.value.substitute;
-      this.reading.dateOfReading = this.form.value.dateOfReading;
-      
+      this.reading.dateOfReading = this.formatDate(new Date(this.form.value.dateOfReading))
+
       console.log('updated customer', this.reading);
 
-      //this._customerService.updateCustomer(this.customer).subscribe();
+      this._readingService.updateReading(this.reading).subscribe();
       this.dialogRef.close(this.form.value);
     }
   }
