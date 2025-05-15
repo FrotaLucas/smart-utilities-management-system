130ff69a062e480574e3cfc24c1453485d91fbca
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-15 14:20:21 +0200

    add column edit

--- src/app/components/reading/show-reading/show-reading.component.ts
+++ src/app/components/reading/show-reading/show-reading.component.ts
@@ -5,7 +5,11 @@ import { Router, RouterModule } from '@angular/router';
 import { ReadingService } from '../../../services/reading.service';
 import { HttpClient } from '@angular/common/http';
 
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 import { MatIconModule } from '@angular/material/icon';
+import { T } from '@angular/cdk/keycodes';
+import { EditReadingComponent } from '../edit-reading/edit-reading.component';
 
 @Component({
   selector: 'app-show-reading',
@@ -18,7 +22,7 @@ export class ShowReadingComponent implements OnInit {
 
   listOfReading!: Reading[]
 
-  constructor(private router: Router, private _readingService: ReadingService){
+  constructor(private router: Router, private _readingService: ReadingService, private dialog: MatDialog) {
     //nao poderia inicializar listOfReading aqui dentro
   }
 
@@ -28,22 +32,22 @@ export class ShowReadingComponent implements OnInit {
 
 
   refreshPage(): void {
-    this._readingService.getReadings().subscribe( data => 
+    this._readingService.getReadings().subscribe(data =>
       this.listOfReading = data
     );
   }
 
-  deleteReading(uuid: string){
-      this._readingService.deleteReading(uuid).subscribe( result => {
-        //console.log('reding deleted');
-        this.refreshPage();
-      }
-        
-      )
+  deleteReading(uuid: string) {
+    this._readingService.deleteReading(uuid).subscribe(result => {
+      //console.log('reding deleted');
+      this.refreshPage();
+    }
+
+    )
   }
 
-  editReading(){
-
+  editReading() {
+    this.dialog.open(EditReadingComponent)
   }
 
   navigateTo(): void {
