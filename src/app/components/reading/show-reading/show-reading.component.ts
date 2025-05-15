9e1415e8011859ef2c46ef17eb84822009201756
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-15 12:51:59 +0200

    implement reading service

--- src/app/components/reading/show-reading/show-reading.component.ts
+++ src/app/components/reading/show-reading/show-reading.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Reading } from '../../../interfaces/reading';
-import { listOfreading } from '../../../shared/list-readings';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { ReadingService } from '../../../services/reading.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-reading',
@@ -10,14 +11,24 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './show-reading.component.html',
   styleUrl: './show-reading.component.css'
 })
-export class ShowReadingComponent {
 
-  myReading: Reading[] = listOfreading;
+export class ShowReadingComponent implements OnInit {
 
-  constructor(private router: Router){}
+  listOfReading!: Reading[]
+
+  constructor(private router: Router, private _readingService: ReadingService){
+    //nao poderia inicializar listOfReading aqui dentro
+  }
 
   ngOnInit(): void {
-    this.myReading = listOfreading;
+    this.refreshPage();
+  }
+
+
+  refreshPage(): void {
+    this._readingService.getReadings().subscribe( data => 
+      this.listOfReading = data
+    );
   }
 
   navigateTo(): void {
