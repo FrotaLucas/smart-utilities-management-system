df461c2791260279ea7085586075ef4d0c7e9d98
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-15 21:45:11 +0200

    implement service to call addCustomer

--- src/app/components/reading/add-reading/add-reading.component.ts
+++ src/app/components/reading/add-reading/add-reading.component.ts
@@ -5,12 +5,14 @@ import { Reading } from '../../../interfaces/reading';
 import { ReadingService } from '../../../services/reading.service';
 
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AddCustomerComponent } from '../../customer/add-customer/add-customer.component';
 
 @Component({
   selector: 'app-add-reading',
@@ -21,28 +23,30 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatButtonModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
   ],
   templateUrl: './add-reading.component.html',
   styleUrl: './add-reading.component.css'
 })
-export class AddReadingComponent implements OnInit{
+export class AddReadingComponent implements OnInit {
 
   form!: FormGroup;
   mylist: Reading[] = listOfreading;
 
-  constructor(private fb: FormBuilder, private _readingService: ReadingService){}
+  constructor(private fb: FormBuilder, private _readingService: ReadingService, 
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.form = this.fb
-    .group({meterId: ['', Validators.required],
-      customerId: [''],
-      kindOfMeter: ['', Validators.required],
-      meterCount: ['', Validators.required],
-      comment: [''],  //nao requerido!!
-      dateOfReading: ['', Validators.required],
-    }
-    )
+      .group({
+        meterId: ['', Validators.required],
+        customerId: [''],
+        kindOfMeter: ['', Validators.required],
+        meterCount: ['', Validators.required],
+        comment: [''],  //nao requerido!!
+        dateOfReading: ['', Validators.required],
+      }
+      )
   }
 
   formatDate(date: Date): string {
@@ -53,32 +57,32 @@ export class AddReadingComponent implements OnInit{
   }
 
   onSubmit(): void {
-    if(this.form.valid){
-      const reading: Reading = this.form.value;
-      reading.substitute = false;
-
-      if (!reading.customer) {
-      reading.customer = {
-        id: 0,
-        uuid: '',
-        firstName: '',
-        lastName: '',
-        birthDate: '',
-        gender: ''
+    if (this.form.valid) {
+      const reading: Reading = {
+        ...this.form.value,
+        substitute: false,
+        customer: {
+          id: this.form.value.customerId,
+          gender: "M",
+          birthDate: "1800-01-01"
+        }
       };
-    }
 
-      reading.customer.id = this.form.value.customerId;
-      reading.customer.birthDate = "1990-01-01";
-      reading.customer.gender = "M";
+      reading.dateOfReading = this.formatDate(new Date(reading.dateOfReading));
 
-      reading.dateOfReading = this.formatDate( new Date(reading.dateOfReading));
+      if( !this.form.value.customerId){
+        console.log('no user');
+        this.dialog.open(AddCustomerComponent, {
+          width: '600px',
+        });
+      }
 
-      this._readingService.addReading(reading).subscribe();
+      else{
+        this._readingService.addReading(reading).subscribe();
+        console.log('new reading addes', reading);
+      }
 
-      //this.mylist.push(reading);
-      console.log('new reading addes', reading);
     }
   }
- 
+
 }
