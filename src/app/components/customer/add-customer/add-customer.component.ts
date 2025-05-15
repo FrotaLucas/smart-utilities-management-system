2faafc80b9749aad42aae287e4a83359b11a5af0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-16 00:03:45 +0200

    implement new scenario with existed customer

--- src/app/components/customer/add-customer/add-customer.component.ts
+++ src/app/components/customer/add-customer/add-customer.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { list } from '../../../shared/list-customers';
 import { CommonModule } from '@angular/common';
 import { Customer } from '../../../interfaces/customer';
+import { Reading } from '../../../interfaces/reading';
 import { CustomerService } from '../../../services/customer.service';
+import { ReadingService } from '../../../services/reading.service';
 
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -11,6 +14,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-customer',
@@ -22,7 +26,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIcon
   ],  //it works without CommomMOdule and RouterModule
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css' //need to be plural here ?
@@ -33,9 +38,18 @@ export class AddCustomerComponent implements OnInit {
   //why this ! symbol??
   form!: FormGroup;
   myList: Customer[] = list;
+  isNewCustomer: boolean = true;
+  reading: Reading | null = null;
 
   //pq formgroup nao entra tbm no construtor?
-  constructor(private fb: FormBuilder, private _customerService: CustomerService) { }
+  constructor(private fb: FormBuilder, private _customerService: CustomerService,
+    private _readingService: ReadingService,
+    @Optional() @Inject(MAT_DIALOG_DATA) private data: { reading: Reading | null, isNewcustomer: boolean },
+    @Optional() private dialogRef: MatDialogRef<AddCustomerComponent>) {
+    //this.reading = this.data.reading || null;
+    this.reading = this.data.reading;
+    this.isNewCustomer = this.data.isNewcustomer;
+  }
 
   ngOnInit(): void {
     this.form = this.fb
@@ -57,13 +71,25 @@ export class AddCustomerComponent implements OnInit {
   onSubmit(): void {
     if (this.form.valid) {
       const customer: Customer = this.form.value;
-
-      // Converter birthDate para string no formato desejado
       customer.birthDate = this.formatDate(new Date(customer.birthDate));
 
-      this._customerService.addCustomer(customer).subscribe(customer =>
-        console.log('new customer added', customer)
-      );
+      //se existir esse componente abrir e  reading tiver algum valor
+      if (this.data.reading) {
+        this.data.reading.customer.firstName = customer.firstName;
+        this.data.reading.customer.lastName = customer.lastName;
+        this.data.reading.customer.gender = customer.gender;
+        this.data.reading.customer.birthDate = customer.birthDate;
+
+        this._readingService.addReading(this.data.reading).subscribe( () => {
+          this.dialogRef.close()
+        });
+      }
+
+      else {
+        this._customerService.addCustomer(customer).subscribe(customer =>
+          console.log('new customer added', customer)
+        );
+      }
     }
   }
 
