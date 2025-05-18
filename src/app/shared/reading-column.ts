220eda07bb4519374a22a66345d6e837e1540c33
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-18 21:29:50 +0200

    fix colum names

--- src/app/shared/reading-column.ts
+++ src/app/shared/reading-column.ts
@@ -1,12 +1,13 @@
 const readingColumns: string[] = [
     "kindOfMeter",
-        "customerId",
-        "comment",
-        "meterCount",
-        "meterId",
-        "substitute",
-        "dateOfReading"
+    "customerId",
+    "comment",
+    "meterCount",
+    "meterId",
+    "substitute",
+    "dateOfReading",
+    "edit"
 ];
 
 
-export {readingColumns};
+export { readingColumns };
