234cc97c37d7195223e354ec8254d212c78921fa
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-19 17:17:24 +0200

    solution with DatePipe

--- src/app/components/customer/add-customer/add-customer.component.ts
+++ src/app/components/customer/add-customer/add-customer.component.ts
@@ -6,6 +6,7 @@ import { Reading } from '../../../interfaces/reading';
 import { CustomerService } from '../../../services/customer.service';
 import { ReadingService } from '../../../services/reading.service';
 
+import { DatePipe } from '@angular/common';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -30,6 +31,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
     MatButtonModule,
     MatIcon
   ],  //it works without CommomMOdule and RouterModule
+  providers: [DatePipe],
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css' //need to be plural here ?
 })
@@ -46,10 +48,12 @@ export class AddCustomerComponent implements OnInit {
   constructor(private fb: FormBuilder, private _customerService: CustomerService,
     private _readingService: ReadingService,
     @Optional() @Inject(MAT_DIALOG_DATA) private data: { reading: Reading | null, isNewCustomer: boolean } | null,
-    @Optional() private dialogRef: MatDialogRef<AddCustomerComponent>, private snackBar: MatSnackBar) {
-    //this.reading = this.data.reading || null;
+    @Optional() private dialogRef: MatDialogRef<AddCustomerComponent>,
+    private snackBar: MatSnackBar, private datePipe: DatePipe) {
+
     this.reading = this.data?.reading ?? null
     this.isNewCustomer = this.data?.isNewCustomer ?? true;
+
   }
 
   ngOnInit(): void {
@@ -62,24 +66,19 @@ export class AddCustomerComponent implements OnInit {
       })
   }
 
-  formatDate(date: Date): string {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // mês começa em 0
-    const year = date.getFullYear();
-    return `${year}-${month}-${day}`;
-  }
-
   onSubmit(): void {
     if (this.form.valid) {
       const customer: Customer = this.form.value;
-      customer.birthDate = this.formatDate(new Date(customer.birthDate));
+      const formattedDate = this.datePipe.transform(customer.birthDate, 'yyyy-MM-dd');
+      customer.birthDate = formattedDate as any;
 
-      //se existir esse componente abrir e  reading tiver algum valor
       if (this.data?.reading) {
         this.data.reading.customer.firstName = customer.firstName;
         this.data.reading.customer.lastName = customer.lastName;
         this.data.reading.customer.gender = customer.gender;
         this.data.reading.customer.birthDate = customer.birthDate;
+       
+        console.log(this.data.reading.customer);
 
         this._readingService.addReading(this.data.reading).subscribe(
           {
@@ -94,10 +93,10 @@ export class AddCustomerComponent implements OnInit {
         this._customerService.addCustomer(customer).subscribe();
 
         this.form.reset();
-        this.snackBar.open("customer added successfully", "", {duration: 2000})
+        this.snackBar.open("customer added successfully", "", { duration: 2000 })
       }
-      
-      
+
+
 
 
 
