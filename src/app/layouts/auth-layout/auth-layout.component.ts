2fc10cd8123c21a0b61fe86fef99f35ab064edca
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-21 08:42:45 +0200

    create authservice

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
-export class AuthLayoutComponent {
+export class AuthLayoutComponent implements OnInit{
 
+  constructor(router: Router){
+
+  }
+  ngOnInit(): void {
+      
+  }
 }
