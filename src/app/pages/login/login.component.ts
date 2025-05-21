fbbd0bab18d1cd455df8d450b9307c5806820f61
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-21 08:50:41 +0200

    implement logic of Logincomponent

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth-service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+  errorMsg: string = '';
+
+  constructor(private router: Router, private auth: AuthService ){
+
+  }
+
+  login(): void {
+    const result = this.auth.login(this.username, this.password);
+    if(result){
+      this.router.navigate(['/customer']);
+    } else {
+       this.errorMsg = 'error on login';
+    }
+  }
 }
