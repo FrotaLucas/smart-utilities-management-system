a4600704e5f0baa5eda80ad3d5c7d85e59d69412
FrotaLucas <lucasdiasfrota@gmail.com>
2025-05-21 08:48:18 +0200

    implement ts file for mainLayoutcomponent

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule} from '@angular/router';
+
+import { DatabaseService } from '../../services/database.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit{
 
-}
+   title = 'final_project_frontend';
+  
+  
+    constructor(private dataBaseService: DatabaseService){
+  
+    }
+  
+    ngOnInit(): void {
+        
+    }
+  
+    resetDatabase(): void{
+      this.dataBaseService.deleteDatabase().subscribe();
+      console.log("delete");
+    }
+  }
+  
+
