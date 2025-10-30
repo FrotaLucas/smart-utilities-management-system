c8189d1ff053a766f73141c912c4545ddb63e7cd
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-30 22:48:02 +0100

    add comment

--- src/app/auth/auth-service.ts
+++ src/app/auth/auth-service.ts
@@ -10,6 +10,7 @@ export class AuthService {
     private isAuthtenticated = new BehaviorSubject<boolean>(false);
 
     validateCredentials(user: string, password: string): boolean { //nao poderia ser void e ?
+        //only for test
         if (user == 'admin@hotmail.com' && password == 'admin') {
             this.isAuthtenticated.next(true);
             return true;
